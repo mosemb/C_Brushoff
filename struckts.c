79ce7ba1f56e7e7e8d75b962566e719c2d365632
mosemb <mosejava@gmail.com>
2021-11-27 00:23:14 +0100

    more stuff

--- struckts.c
+++ struckts.c
@@ -1,9 +1,29 @@
-#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 struct Student {
     char name [50];
     char major [50];
-    int age; 
+    double age; 
+    double gpa;
 };
+
+int main(){
+
+    struct Student student;
+    student.age = 50;
+    student.gpa = 3.8;
+    strcpy(student.major,"Bussines Economics ");
+    strcpy(student.name, "James Brown");
+    
+
+printf("Student major: %s \n",student.major);
+printf("Student name: %s \n",student.name);
+printf("Student gpa: %f \n",student.gpa);
+printf("Student age: %f \n",student.age);
+
+return 0;
+
+}
 
 
